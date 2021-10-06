fe82231a8a98d7f0af4a50f15e13c717d441928d
Silvan Sievers <silvan.sievers@unibas.ch>
2021-10-06 20:41:39 +0200

    [structural-symmetries-pruning] add fancy scatter plots

--- experiments/structural-symmetries-pruning/2021-09-17-ms.py
+++ experiments/structural-symmetries-pruning/2021-09-17-ms.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from collections import defaultdict
 import itertools
 import os
 
@@ -7,6 +8,7 @@ from lab.environments import LocalEnvironment, BaselSlurmEnvironment
 from lab.reports import Attribute, arithmetic_mean, geometric_mean
 
 from downward.reports.compare import ComparativeReport
+from downward.reports.scatter import ScatterPlotReport
 
 import common_setup
 from common_setup import IssueConfig, IssueExperiment
@@ -77,16 +79,88 @@ exp.add_absolute_report_step(
     attributes=attributes,
 )
 
+dks_ms_prunedead = '{}-dks-ms-prunedead'.format(REVISION)
+dks_ms_pruneirrelevant = '{}-dks-ms-pruneirrelevant'.format(REVISION)
+oss_ms_pruneirrelevant = '{}-oss-ms-pruneirrelevant'.format(REVISION)
+algorithm_pairs = [
+    (dks_ms_prunedead, dks_ms_pruneirrelevant),
+    (dks_ms_prunedead, oss_ms_pruneirrelevant),
+    (dks_ms_pruneirrelevant, oss_ms_pruneirrelevant),
+]
 exp.add_report(
     ComparativeReport(
-        algorithm_pairs=[
-            ('{}-dks-ms-prunedead'.format(REVISION),'{}-dks-ms-pruneirrelevant'.format(REVISION)),
-            ('{}-dks-ms-prunedead'.format(REVISION),'{}-oss-ms-pruneirrelevant'.format(REVISION)),
-            ('{}-dks-ms-pruneirrelevant'.format(REVISION),'{}-oss-ms-pruneirrelevant'.format(REVISION)),
-        ],
+        algorithm_pairs=algorithm_pairs,
         attributes=attributes,
     ),
     outfile=os.path.join(exp.eval_dir, exp.name+"-compare.html")
+)
+
+class FilterDomains:
+    def __init__(self, algo1, algo2, attribute, factor):
+        self.algo1 = algo1
+        self.algo2 = algo2
+        self.attribute = attribute
+        self.factor = factor
+        self.domain_problem_algo_attribute = defaultdict(dict)
+        self.processed = False
+
+    def collect_data(self, props):
+        algo = props['algorithm']
+        if algo != self.algo1 and algo != self.algo2:
+            return False
+        domain = props['domain']
+        problem = props['problem']
+        attribute = props.get(self.attribute, float('inf'))
+        if problem not in self.domain_problem_algo_attribute[domain]:
+            self.domain_problem_algo_attribute[domain][problem] = dict()
+        self.domain_problem_algo_attribute[domain][problem][algo] = attribute
+        return True
+
+    def process_data(self, props):
+        if not self.processed:
+            self.processed = True
+            self.interesting_domains = set()
+            for domain, rest in self.domain_problem_algo_attribute.items():
+                for problem, algo_expansions in rest.items():
+                    assert len(algo_expansions) == 2, algo_expansions
+                    assert self.algo1 in algo_expansions
+                    assert self.algo2 in algo_expansions
+                    if algo_expansions[self.algo1] > self.factor * algo_expansions[self.algo2] or algo_expansions[self.algo2] > self.factor * algo_expansions[self.algo1]:
+                        # print(f"{domain} is interesting; {algo_expansions[self.algo1]}, {algo_expansions[self.algo2]}")
+                        self.interesting_domains.add(domain)
+        return True
+
+    def filter_domains(self, props):
+        return props['domain'] in self.interesting_domains
+
+    def get_category(self, run1, run2):
+        domain = run1['domain']
+        assert domain == run2['domain']
+        return domain
+
+for algo1, algo2 in algorithm_pairs:
+    filter_domains = FilterDomains(algo1, algo2, 'expansions_until_last_jump', 10)
+    exp.add_report(
+        ScatterPlotReport(
+            filter=[filter_domains.collect_data, filter_domains.process_data, filter_domains.filter_domains],
+            attributes=["expansions_until_last_jump"],
+            relative=False,
+            get_category=filter_domains.get_category,
+            format='png',
+        ),
+        outfile=os.path.join(exp.eval_dir, algo1.replace('{}-'.format(REVISION), '') + "-" + algo2.replace('{}-'.format(REVISION), '') + "-expansions"),
+    )
+
+filter_domains = FilterDomains(dks_ms_pruneirrelevant, oss_ms_pruneirrelevant, 'memory', 2)
+exp.add_report(
+    ScatterPlotReport(
+        filter=[filter_domains.collect_data, filter_domains.process_data, filter_domains.filter_domains],
+        attributes=["memory"],
+        relative=False,
+        get_category=filter_domains.get_category,
+        format='png',
+    ),
+    outfile=os.path.join(exp.eval_dir, dks_ms_pruneirrelevant.replace('{}-'.format(REVISION), '') + "-" + oss_ms_pruneirrelevant.replace('{}-'.format(REVISION), '') + "-memory"),
 )
 
 exp.run_steps()
